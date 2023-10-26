fa18a21b66d8d211a741a65c7926e307691aad0c
youngjae <wlsdudwo616@naver.com>
2023-10-26 16:55:20 +0900

    10/28

---
+++ src/pages/community/index.js
@@ -0,0 +1,15 @@
+import MainLayout from "../layout/MainLayout";
+import Commu from "../../components/Community/Commu";
+
+function Community() {
+
+  return(
+      <MainLayout>
+        <Commu/>
+      </MainLayout>
+
+      )
+
+}
+
+export default Community;
